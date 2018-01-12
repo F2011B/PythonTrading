1d6114f23e584c4355366ef930b40935e945b4df
Dark Light <darkshadow201710[at]gmx.de>
2018-01-12 22:02:18 +0100

    Changed a comment

--- PandasCore/OZ.py
+++ PandasCore/OZ.py
@@ -9,9 +9,7 @@ import os
 import inspect
 import unittest
 
-
-
-
+#This is a comment
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
